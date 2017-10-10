1197fa99e0c9a521bfb80743cd1bc03e840d9875
Lucas Simoni <lucas.simoni10@gmail.com>
2017-10-10 18:20:45 -0300

    Instalador do everyuse

---
+++ install.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# Codado por: Lucas Simoni <lucas.simoni10@gmail.com>
+#
+# Programa com finalidade de aprendizado.
+# Todas as suas ações utilizando o everyuse são responsabilidades SUA.
+import os
+
+cminst = '\033[1m\033[33meveryuse\033[0;0m» '
+if os.getuid() != 0:
+    print(cminst+'Erro!')
+    print(cminst+'Execute como administrador.')
+    exit(0)
+
+if os.path.exists('/usr/bin/everyuse'):
+    print(cminst+'Você já possui o everyuse!\n Tente executar: sudo everyuse')
+else:
+    yorn = input(cminst+'Deseja fazer a instalação do programa? [S/n] ')
+    if yorn.lower() == 's' or yorn.lower() != 's' and yorn.lower() != 'n':
+        print(cminst+'Executando a instalação...')
+        if os.path.exists('everyuse.py') and os.path.exists('modulos/'):
+            try:
+                print(cminst+'Deseja mover o programa para poder utilizar ele com apenas o comando:')
+                print('    sudo everyuse')
+                desejamover = input(cminst+'? [S/n]')
+                if desejamover.lower() == 's' or desejamover.lower() != 's' and desejamover.lower() != 'n':
+                    os.system('sudo mv everyuse.py /usr/bin/everyuse')
+                    os.system('sudo chmod +x /usr/bin/everyuse')
+                    os.system('sudo mv modulos/ /usr/bin/modulos')
+                    print(' [*] Arquivos movido com sucesso para /usr/bin/')
+                    print(' [*] Para executar, digite: sudo everyuse')
+                elif desejamover.lower() == 'n':
+                    os.system('sudo mv everyuse.py everyuse')
+                    print(' [*] Arquivo renomeado para everyuse')
+            except:
+                print('Algum erro aconteceu!\n Tente usar o comando: git clone https://github.com/r00tinker/everyuse')
